774e3a97ec93e2ceb56622d9917ae8d1e1dbbf9a
Noah Beito <noah.beito@gmail.com>
2023-07-19 16:56:53 -0700

    search successfully retrieves and displays books based on search input

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -5,11 +5,19 @@ import Button from './Button';
 
 export default function Search() {
   const [books, setBooks] = useState([]);
+  const [searchText, setSearchText] = useState('');
 
   const onSearchHandler = () => {
-    axios.get('/books')
+    console.log('----', searchText);
+    axios.get('/books', {
+      params: {
+        author: 'unknown',
+        quote: searchText,
+      },
+    })
       .then((res) => {
-        setBooks(res);
+        console.log(res, res.data);
+        setBooks(res.data);
       })
       .catch((err) => {
         console.log(err);
@@ -19,13 +27,15 @@ export default function Search() {
   return (
     <>
       <h1 className="Search">Search Icon</h1>
-      <input type="text" />
-      <Button name="Search" clickHandler={onSearchHandler} />
-      {
-        books.map((book) => (
-          <Book book={book} key={book.id} />
-        ))
-      }
+      <input type="text" onChange={(e) => setSearchText(e.target.value)} />
+      <button type="submit" onClick={() => onSearchHandler()}>Search</button>
+      <div className="search-books-container">
+        {
+          books.map((book) => (
+            <Book book={book} key={book.id} />
+          ))
+        }
+      </div>
     </>
   );
 }
